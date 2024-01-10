17424627f1add92161836db0fa454bafcc0342b2
Szymon Czap <127615744+SzymonCzap@users.noreply.github.com>
2024-01-10 19:21:10 +0100

    first commit.

--- SolarWatch/SolarWatch/obj/Debug/net7.0/SolarWatch.AssemblyInfo.cs
+++ SolarWatch/SolarWatch/obj/Debug/net7.0/SolarWatch.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SolarWatch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ada787814412d4fecc2120e48429002bf3e45f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SolarWatch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SolarWatch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
